36fdcd182b702e2ff5e06c16061d9549cf58a0b3
Kevin Joseph Galinsky <galinsky@fas.harvard.edu>
2013-07-08 18:45:04 -0400

    read_barcodes added

--- lib/COIL/Barcode.pm
+++ lib/COIL/Barcode.pm
@@ -5,6 +5,8 @@ use warnings;
 
 use Carp;
 
+use COIL;
+
 =head1 FUNCTIONS
 
 =head2 validate_barcode
@@ -22,7 +24,7 @@ Validate the barcodes and croak if bad.
 =cut
 
 sub validate_barcode {
-    croak 'Barcodes are arrayrefs' unless ( ref( $_[0] ) eq 'ARRAY' );
+    croak 'Barcode not arrayref' unless ( ref( $_[0] ) eq 'ARRAY' );
 
     foreach my $allele ( @{ $_[0] } ) {
         croak qq{Invalid allele "$allele"} unless ( $allele =~ m/^[ACGTNX]$/ );
@@ -33,6 +35,28 @@ sub validate_barcodes {
     foreach my $barcode ( @{ $_[0] } ) {
         validate_barcode($barcode);
     }
+}
+
+=head2 read_barcodes
+
+    my $barcodes = read_barcodes($filename);
+    my $barcodes = read_barcodes($filehandle);
+
+Read barcodes from file.
+
+=cut
+
+sub read_barcodes {
+    my $fh = COIL::_fh( \@_ );
+
+    my @barcodes;
+    foreach local $_ (<$fh>) {
+        my ($barcode_str) = (split)[-1];
+        push @barcodes, [ split //, $barcode_str ];
+    }
+
+    validate_barcodes( \@barcodes );
+    return \@barcodes;
 }
 
 1;
