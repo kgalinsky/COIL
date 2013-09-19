2b4bcbd266695ad4da6676b9f7dc380946e21297
Kevin Galinsky <kgalinsky@gmail.com>
2013-09-19 17:18:33 -0400

    Pair uses a new structure and unit object.

--- lib/COIL/Tally/Pair.pm
+++ lib/COIL/Tally/Pair.pm
@@ -24,43 +24,52 @@ use COIL '_fh';
 
 =cut
 
-# A COIL::Tally::Pair object is a 2-dimensional array of counts
+# A COIL::Tally::Pair object is an array of counts storing the bottom-left
+# off-diagonal elements of a matrix. The numbering is:
 #
-# $CTP->[$i][$j] = [ [ $NN_ij, $Nn_ij ], [ $nN_ij, $nn_ij ] ]
+#         0   1   2   3
+#       +---+---+---+---+---+
+#     0 | X |   |   |   |...|
+# M =   +---+---+---+---+---+
+#     1 | 0 | X |   +   +...|
+#       +---+---+---+---+---+
+#     2 | 1 | 2 | X |   |...|
+#       +---+---+---+---+---+
+#     3 | 3 | 4 | 5 | X |...|
+#       +---+---+---+---+---+
+#       |...|...|...|...|...|
+#       +---+---+---+---+---+
+#
+# To map M[i][j] = CTP[k]
+#
+#     k = (i^2)/2-i/2+j
+#
+# $CTP->[$k] = [ [ $NN_ij, $Nn_ij ], [ $nN_ij, $nn_ij ] ]
 
 sub tally_numerics {
     my $class = shift;
     my ($numerics) = validate_pos( @_, $VAL_NUMERICS );
 
-    my $self = bless [
-        map {
-            [ map { [ [ 0, 0 ], [ 0, 0 ] ] } @{ $numerics->[0] } ]
-          } @{ $numerics->[0] }
-
-    ], $class;
+    my $n = $#{ $numerics->[0] };
+    my $self =
+      bless [ map { "${class}::Unit"->new } (0) x ( $n * ( $n + 1 ) / 2 ) ],
+      $class;
 
     foreach my $numeric (@$numerics) {
         next if ( grep { $_ == 2 } @$numeric );    # skip if barcode is poly
 
         # iterate through each SNP
-        for ( my $i = 0 ; $i < @$numeric ; $i++ ) {
+        for ( my $i = 1 ; $i < @$numeric ; $i++ ) {
             my $ni = $numeric->[$i];
             next if ( $ni == 3 );                  # skip failed assays
 
-            $self->[$i][$i][$ni][$ni]++;           # increment diagonal
+            my $offset = $i * ( $i - 1 ) / 2;
 
-            for ( my $j = $i + 1 ; $j < @$numeric ; $j++ ) {
+            for ( my $j = 0 ; $j < $i ; $j++ ) {
                 my $nj = $numeric->[$j];
                 next if ( $nj == 3 );              # skip failed assays
-                $self->[$i][$j][$ni][$nj]++;
+                $self->[ $offset + $j ][$ni][$nj]++;
             }
-        }
-    }
-
-    # mirror
-    for ( my $i = 1 ; $i < @$self ; $i++ ) {
-        for ( my $j = 0 ; $j < $i ; $j++ ) {
-            $self->[$i][$j] = $self->[$j][$i];
         }
     }
 
@@ -84,30 +93,25 @@ use Text::NSP::Measures::2D::Fisher::twotailed;
 
 sub Fisher {
     my $self = shift;
-
-    my @stats;
-    for ( my $i = 0 ; $i < $#$self ; $i++ ) {
-        for ( my $j = $i + 1 ; $j < @$self ; $j++ ) {
-            my $n11 = $self->[$i][$j][0][0];
-            my $n12 = $self->[$i][$j][0][1];
-            my $n21 = $self->[$i][$j][1][0];
-            my $n22 = $self->[$i][$j][1][1];
+    return [
+        map {
+            my $n11 = $_->[0][0];
+            my $n12 = $_->[0][1];
+            my $n21 = $_->[1][0];
+            my $n22 = $_->[1][1];
 
             my $n1p = $n11 + $n12;
             my $np1 = $n11 + $n21;
             my $npp = $n11 + $n12 + $n21 + $n22;
 
-            $stats[$i][$j] = $stats[$j][$i] =
-              Text::NSP::Measures::2D::Fisher::twotailed::calculateStatistic(
+            Text::NSP::Measures::2D::Fisher::twotailed::calculateStatistic(
                 n11 => $n11,
                 n1p => $n1p,
                 np1 => $np1,
                 npp => $npp
-              );
-        }
-    }
-
-    return \@stats;
+            );
+        } @$self
+    ];
 }
 
 =head2 write
@@ -120,17 +124,27 @@ sub write {
 
     local $, = "\t";
     local $\ = "\n";
-    for ( my $i = 0 ; $i < $#$self ; $i++ ) {
-        for ( my $j = $i + 1 ; $j < @$self ; $j++ ) {
-            my $t = $self->[$i][$j];
-            print $i, $j,
-                '['
-              . $t->[0][0] . ':'
-              . $t->[0][1] . '/'
-              . $t->[1][0] . ':'
-              . $t->[1][1] . ']';
-        }
+
+    my ( $i, $j ) = ( 1, 0 );
+    foreach my $t (@$self) {
+        print $i, $j, $t;
+
+        $j++;
+        if ( $i == $j ) { $i++; $j = 0; }
     }
 }
+
+package COIL::Tally::Pair::Unit;
+
+use strict;
+use warnings;
+
+sub new {
+    my $class = shift;
+    bless [ [ 0, 0 ], [ 0, 0 ] ], $class;
+}
+
+use overload '""' => \&to_string;
+sub to_string { "[$_[0][0][0]:$_[0][0][1]/$_[0][1][0]:$_[0][1][1]]" }
 
 1;
